5b7142960f1ae82380fed530de6e396246eca5fc
Dan Wagner <danwagner@ksu.edu>
2016-11-03 18:21:54 -0500

    comitt message

--- json_chat_server_client.py
+++ json_chat_server_client.py
@@ -4,9 +4,8 @@ import json
 from contextlib import suppress
 
 
-@asyncio.coroutine
-def send_encoded():
-    websocket = yield from websockets.connect('ws://localhost:8054/')
+async def send_encoded():
+    websocket = await websockets.connect('ws://localhost:8054/')
 
     try:
         while True:
@@ -15,13 +14,13 @@ def send_encoded():
                 'msg': message,
             }
             d = json.dumps(encoded_message) #encodes the dictionary "d"
-            yield from websocket.send(d)
+            await websocket.send(d)
 
-            with suppress(asyncio.TimeroutError):
+            with suppress(asyncio.TimeoutError):
                 msg = await asyncio.wait_for(websocket.recv(), .1)
                 json.loads(msg)
     finally:
 
-        yield from websocket.close()
+        await websocket.close()
 
 asyncio.get_event_loop().run_until_complete(send_encoded())
