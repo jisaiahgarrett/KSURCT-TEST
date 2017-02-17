da968158b54682e5b8086b4f0864075e7668c13a
jisaiahgarrett <jisaiahgarrett@gmail.com>
2017-02-16 20:44:25 -0600

    Working client for Pi connectivity.

--- CL_client.py
+++ CL_client.py
@@ -9,14 +9,22 @@ controller = Controller(0)
 
 async def SendMessage():
     websocket = await websockets.connect('ws://10.243.193.47:8055/')
+    #this is old IP 'ws://10.243.242.98:8055/' Isaiah changed on 2/6/17
     # zerotier IP of server
     # was ws://localhost:8055/
     try:
         while True:
             controller.update()
-            # The x,a,b,y-----------------------------------
-            # message = "{} {} {} {}".format(controller.x(), controller.y(), controller.b(), controller.a())
-
+            # The x,a,b,y-----------------------------------  Binary representation: abxy
+            message = "{} {} {} {}".format(controller.x(), controller.y(), controller.b(), controller.a())
+            if controller.x() == 1:
+                message = 0b0010
+            elif controller.y() == 1:
+                message = 0b0001
+            elif controller.a() == 1:
+                message = 0b1000
+            elif controller.b() == 1:
+                message = 0b0100
             # The left and right analog sticks--------------
             # message = "{} {} {} {}".format(controller.left_y(), controller.left_x(), controller.right_x(), controller.right_y())
 
@@ -24,8 +32,8 @@ async def SendMessage():
             # message = "{} {}".format(controller.left_trigger(), controller.right_trigger())
 
             # The left and right bumpers
-            message = "{} {}".format(controller.left_bumper(), controller.right_bumper())
-            if(message):
+            # message = "{} {}".format(controller.left_bumper(), controller.right_bumper())
+            if(message): #  and (controller.left_bumper() is not False)):
                 await websocket.send(str(message))
             # else:
             #     await websocket.send("false")
